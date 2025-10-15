29173fdfbbef97e3537a86ec98d2f8da5c97b222
shashank-poola <shashankpoola123@gmail.com>
2025-10-15 22:06:01 +0530

    drop in threadpool

--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,48 @@ enum Message {
 }
 
 impl ThreadPool{
+    pub fn new (size: usize) -> ThreadPool {
+        assert!(size>0);
+
+        let (sender, receiver) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size(
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+
+        )
+        ThreadPool {Workers, sender}
+    }
+
+    pub fn execute<F>(&self, f:F)
+    where 
+        F: FnOnce() + Send + 'static
+        {
+            let job = Box::new(f);
+            self.sender.send(Message::NewJob(job)).unwrap();
+        }
+
     
+}
+
+impl Drop for ThreadPool{
+    fn Drop(&mut self) {
+            println!("Sending terminate message to all workers.");
+
+            for _ in &self.workers{
+                self.sender.send(Message::Terminate).unwrap();
+            }
+            println!("Shutting down all worker";
+            for worker in &mut self.workers{
+                println!("Shutting down all the worker {}", worker.id);
+                if let Some(thread) = worker.thread.take(){
+                    thread.join().unwrap();
+                }
+            }
+        )
+    }
+
+    struct Worker{
+        id: usize;
+        thread: Option<thread:: JoinHandle<()>>
+    }
 }
